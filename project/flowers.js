286dbf962ddf2f3b52d68a505295593cd2d4664b
asianjesusfr <157668714+asianjesusfr@users.noreply.github.com>
2025-04-07 21:10:02 +0000

    finished flower data code

--- project/flowers.js
+++ project/flowers.js
@@ -1,5 +1,6 @@
 let data;
 let m = 0
+let y = 100
 
 function preload() {
   data = loadJSON("flower.json");
@@ -11,9 +12,12 @@ function setup() {
   
   let flowers = data.flowers;
  
+  for(let m=0; m<4; m++){
   fill(flowers[m].r,flowers[m].g,flowers[m].b);
-  ellipse(150,150,150,150);
+  ellipse(150,y,150,150);
   stroke(0);
   textSize(22);
-  text(flowers[m].name, 100, 150);
+  text(flowers[m].name, 100, y);
+  y=y+75
+  }
 }
